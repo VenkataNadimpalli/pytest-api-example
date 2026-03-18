c08fa1881eee6ece0b5f54f9f3ba821afcc0b081
VenkataNadimpalli <50088742+VenkataNadimpalli@users.noreply.github.com>
2026-03-18 00:55:35 -0400

    test store patch task
    Implemented test for PATCH request to update order status and added fixture for creating test data.

--- test_store.py
+++ test_store.py
@@ -1,9 +1,5 @@
-from jsonschema import validate
-import pytest
-import schemas
 import api_helpers
-from hamcrest import assert_that, contains_string, is_
-
+import pytest
 '''
 TODO: Finish this test by...
 1) Creating a function to test the PATCH request /store/order/{order_id}
@@ -12,5 +8,21 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+
+
+@pytest.fixturea
+def another_order():
+    payload = {"pet_id": 0}
+    response = api_helpers.post_api_data("/store/order", payload)
+    assert response.status_code == 201
+    return response.json()["id"]
+
+
+def test_patch_order_by_id(another_order):
+    test_endpoint = f"/store/order/{another_order}"
+    payload = {"status": "sold"}
+    response = api_helpers.patch_api_data(test_endpoint, payload)
+
+    assert response.status_code == 200
+    body = response.json()
+    print("patch_message:", body)
